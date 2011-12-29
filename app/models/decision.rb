031d1f31a70fd4b2e6ec606647fc389804ee4b33
Steve Richert <steve.richert@gmail.com>
2011-12-29 12:09:38 -0500

    Add a simple decision state machine

--- app/models/decision.rb
+++ app/models/decision.rb
@@ -1,17 +1,34 @@
 class Decision
+  STEPS = %w(question options factors scores answer)
+
   include MongoMapper::Document
 
   key :question, String
+  key :step,     String
   key :token,    String
 
   before_create :set_token, :unless => :token?
+  before_create :set_step, :unless => :step?
 
   def to_param
     token
+  end
+
+  def continue
+    increment_step
+    save
   end
 
   private
     def set_token
       self.token = SecureRandom.hex
     end
-end
+
+    def set_step
+      self.step = STEPS.first
+    end
+
+    def increment_step
+      self.step = STEPS[STEPS.index(step) + 1]
+    end
+  end
