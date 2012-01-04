0da145335ca1e875bfc0b1bbcfdc3eef9341de4f
Steve Richert <steve.richert@gmail.com>
2012-01-04 14:24:52 -0500

    Fix blank value interpretation in integer arrays

--- lib/integer_array.rb
+++ lib/integer_array.rb
@@ -3,8 +3,9 @@ class IntegerArray < Array
     case value
     when Hash then value.inject([]){|a,(k,v)| a[k.to_i] = to_mongo(v); a }
     when Array then value.map{|v| to_mongo(v) }
-    when nil, "" then []
-    else value.to_i
+    when Integer then value
+    when /^\d+$/ then value.to_i
+    else nil
     end
   end
 
